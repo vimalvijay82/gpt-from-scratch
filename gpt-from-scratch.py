f985e80226c327fa541d6513c69b7922b16d8b99
Vimal Vijay <vimalvijay82@gmail.com>
2023-11-27 12:53:47 +0530

    fix: error in model class

--- gpt-from-scratch.py
+++ gpt-from-scratch.py
@@ -95,9 +95,9 @@ class FeedForward(nn.Module):
     def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, n_embd),
+            nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
-            nn.Linear(n_embd, n_embd),
+            nn.Linear(4 * n_embd, n_embd),
         )
 
         def forward(self, x):
@@ -135,8 +135,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B, T, C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
         x = tok_emb + pos_emb # (B, T, C)
-        x = self.sa_heads(x) # apply one head of self-attention
-        x = self.ffwd(x) # B, T, C
+        x = self.blocks(x) # apply one head of self-attention
         logits = self.lm_head(x) # (B, T, vocab_size)
 
         if targets is None:
